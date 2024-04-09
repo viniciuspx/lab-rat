e70692c95bf819020171f592d382cdc4b7f4c8fb
viniciuspx <vinicius.gabriel.px@gmail.com>
2024-04-09 15:27:32 -0300

    guessing game

--- lab-rat/03guessing_game/guessing_game/src/main.rs
+++ lab-rat/03guessing_game/guessing_game/src/main.rs
@@ -1,33 +1,53 @@
 // see toml file too
 // import the standard input/output(io) lib
-use std::io;
+use std::{cmp::Ordering, io};
 
-fn main(){
-    println!("Guess the number!");
+// in rand lib we use rng that generates random numbers
+use rand::Rng;
 
-    println!("Please input your guess.");
+fn main() {
+    // generates a whole random number between 0 ~ 100
+    let secret_number = rand::thread_rng().gen_range(0..=100);
 
-    // creates a mutable variable of the type String
-    let mut guess = String::new();
+    loop {
+        println!("Guess the number!");
+        // creates a mutable variable of the type String
+        let mut guess = String::new();
 
-    // In Rust variables ares immutable by default, i.e. 
-    // constants in value 
-    // mut marks the variable as mutable
+        // In Rust variables are immutable by default, i.e.
+        // constants in value
+        // mut marks the variable as mutable
 
-    // :: indicates the access of the Type
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin()
-       .read_line(&mut guess)
-       .expect("Failed to read line");
+        // :: in this we accessed the module io
+        // stdin() -> standard input method
+        // read_line(&param) -> stores read line in param
+        // &var -> indicates the reference of the var
+        // references are also immutable, thus the &mut
+        // expect -> error handler
 
-    // :: in this we accessed the module io
-    // stdin() -> standard input method
-    // read_line(&param) -> stores read line in param
-    // &var -> indicates the reference of the var
-    // references are also immutable, thus the &mut
-    // expect -> error handler
+        // We "shadow" (shadowing) the last guess var with the new u32 one
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {guess}");
+        // trim() -> removes whitespaces and \n
+        // parse() -> parses the string to the annotated type in this case after the ":" -> u32
 
-    // {var} prints the var value
+        println!("You guessed: {guess}");
+        // println!({var}) prints the var value
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
