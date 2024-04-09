7fbb77e64eb39a9dbc335309e53739ef8e5b805d
viniciuspx <vinicius.gabriel.px@gmail.com>
2024-04-09 13:47:07 -0300

    Removing executables and builds

--- lab-rat/03guessing_game/guessing_game/target/debug/deps/cfg_if-a9f0e8a7b3b5c4c6.d
+++
@@ -1,7 +0,0 @@
-/home/vinicius/dev/lab-rat/lab-rat/03guessing_game/guessing_game/target/debug/deps/libcfg_if-a9f0e8a7b3b5c4c6.rmeta: /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/vinicius/dev/lab-rat/lab-rat/03guessing_game/guessing_game/target/debug/deps/libcfg_if-a9f0e8a7b3b5c4c6.rlib: /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/vinicius/dev/lab-rat/lab-rat/03guessing_game/guessing_game/target/debug/deps/cfg_if-a9f0e8a7b3b5c4c6.d: /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
--- lab-rat/03guessing_game/guessing_game/target/debug/deps/rand_chacha-02d802acc70302cd.d
+++
@@ -1,9 +0,0 @@
-/home/vinicius/dev/lab-rat/lab-rat/03guessing_game/guessing_game/target/debug/deps/librand_chacha-02d802acc70302cd.rmeta: /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/home/vinicius/dev/lab-rat/lab-rat/03guessing_game/guessing_game/target/debug/deps/librand_chacha-02d802acc70302cd.rlib: /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/home/vinicius/dev/lab-rat/lab-rat/03guessing_game/guessing_game/target/debug/deps/rand_chacha-02d802acc70302cd.d: /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs /home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs
-
-/home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/lib.rs:
-/home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/chacha.rs:
-/home/vinicius/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rand_chacha-0.3.1/src/guts.rs:
