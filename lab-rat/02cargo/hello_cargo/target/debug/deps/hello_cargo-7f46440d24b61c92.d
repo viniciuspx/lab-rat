cb1b83a48c4191f6ad508354de5598da871575f8
viniciuspx <vinicius.gabriel.px@gmail.com>
2024-04-09 13:44:21 -0300

    Adding gitignore

--- lab-rat/02cargo/hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92.d
+++ lab-rat/02cargo/hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92.d
@@ -1,5 +1,5 @@
-/home/vinicius/dev/lab-rat/lab-rat/cargo/hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92: src/main.rs
+/home/vinicius/dev/lab-rat/lab-rat/02cargo/hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92: src/main.rs
 
-/home/vinicius/dev/lab-rat/lab-rat/cargo/hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92.d: src/main.rs
+/home/vinicius/dev/lab-rat/lab-rat/02cargo/hello_cargo/target/debug/deps/hello_cargo-7f46440d24b61c92.d: src/main.rs
 
 src/main.rs:
--- lab-rat/02cargo/hello_cargo/target/debug/hello_cargo.d
+++ lab-rat/02cargo/hello_cargo/target/debug/hello_cargo.d
@@ -1,1 +1,1 @@
-/home/vinicius/dev/lab-rat/lab-rat/cargo/hello_cargo/target/debug/hello_cargo: /home/vinicius/dev/lab-rat/lab-rat/cargo/hello_cargo/src/main.rs
+/home/vinicius/dev/lab-rat/lab-rat/02cargo/hello_cargo/target/debug/hello_cargo: /home/vinicius/dev/lab-rat/lab-rat/02cargo/hello_cargo/src/main.rs
