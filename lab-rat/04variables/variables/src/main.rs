585bb4c42ea4585ad5c20acd67e0c4a35771198b
viniciuspx <vinicius.gabriel.px@gmail.com>
2024-04-12 23:46:26 -0300

    variables and shadowing

--- lab-rat/04variables/variables/src/main.rs
+++ lab-rat/04variables/variables/src/main.rs
@@ -1,3 +1,25 @@
+// this simple example is to show the use of mutable variables
 fn main() {
-    println!("Hello, world!");
+    // without the keyword mut the variable cant get assigned again
+    // implcit typing in this case i32
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    // constants cant change value, and must have the type defined in this case u32
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("{THREE_HOURS_IN_SECONDS}");
+
+    // more shadowing
+    let y = 5;
+    let y = y + 1;
+    // different from assigning we are using let again to re-define the varible y
+    // in this way we "shadow" the previous value
+    {
+        // this variable only exists in this scope, once we are out >>puff
+        let y = y * 2;
+        println!("The value of x in the inner scope is {y}");
+    }
+
+    println!("The value of x is: {y}");
 }
